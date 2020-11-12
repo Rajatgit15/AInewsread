776ce30955db7429e709acee0be8a09ea88ba3e2
Rajat9915 <49584553+Rajat9915@users.noreply.github.com>
2020-11-12 23:44:33 +0530

    Updates

---
+++ precache-manifest.ecf861066e95b4d28b14ebe7aad54e9a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a629c306e332c81bb42413853d33536",
+    "url": "/AInewsread/index.html"
+  },
+  {
+    "revision": "2f51fca0520aaf10cd17",
+    "url": "/AInewsread/static/css/main.258c2ef8.chunk.css"
+  },
+  {
+    "revision": "20720c988cee07617c8b",
+    "url": "/AInewsread/static/js/2.b84f5614.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/AInewsread/static/js/2.b84f5614.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f51fca0520aaf10cd17",
+    "url": "/AInewsread/static/js/main.d9d336fe.chunk.js"
+  },
+  {
+    "revision": "3962c95d0bc394f92cc2",
+    "url": "/AInewsread/static/js/runtime-main.ff28fd97.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/AInewsread/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e2311a31a3865a20d4f262776f2176f7",
+    "url": "/AInewsread/static/media/pic.e2311a31.jpg"
+  }
+]);
